57824392f746addb6dfef58cec99ba98e7d2d355
Diego Pallares Git <fercho.182@hotmail.com>
2023-05-29 19:25:47 -0500

    Update paleta colores

--- src/componentes/TaskCounter.js
+++ src/componentes/TaskCounter.js
@@ -1,20 +1,22 @@
 import '../Styles/TaskCounter.css'
 
 const validateAllTaskcomplete = (total, completed) => {
-    if(total === completed){
-      return true
+    if(total === completed && total === 0){
+      return  (<h1 className="TaksCounter" >Crea tu primera tarea</h1>)
+    }else if (total === completed && total > 0 ){
+      return  (<h1 className="TaksCounter" >Felicidades completastes todos tus pendientes</h1>)
     }else{
-      return false
+      return (<h1 className="TaksCounter">Haz completado <span>{completed}</span> de <span>{total}</span> Tareas</h1>)
     }
 }
 
 function TaskCounter({total , completed}){
   
   return (
-    validateAllTaskcomplete(total , completed) ? 
-    (<h1 className="TaksCounter" >Felicidades completastes todos tus pendientes</h1>)
-    :
-    (<h1 className="TaksCounter">Haz completado <span>{completed}</span> de <span>{total}</span> Tareas</h1>)
+    validateAllTaskcomplete(total , completed) //? 
+   // (<h1 className="TaksCounter" >Felicidades completastes todos tus pendientes</h1>)
+    //:
+    //(<h1 className="TaksCounter">Haz completado <span>{completed}</span> de <span>{total}</span> Tareas</h1>)
         
     )
 }
